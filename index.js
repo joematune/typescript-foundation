5f0c6faec76604f55189c243b1788799b753657c
joematune <joematune@gmail.com>
2020-10-27 12:10:17 +0200

    Add class Player with ratingCategory getter

--- index.js
+++ index.js
@@ -1,24 +1,48 @@
 console.log('TypeScript for Chess players');
 class CreatePlayer {
-    constructor(name, ratingValue) {
+    constructor(name, rating) {
+        this.id = Math.floor(Math.random() * 1000).toString();
         this.name = name;
-        this.rating = {
-            category: 'CA',
-            value: ratingValue
-        };
+        this.rating = rating;
     }
-    get isRatingValue() {
-        return this.rating.value > 0;
+    get ratingCategory() {
+        const { rating: r } = this;
+        if (r < 1200) {
+            return 'N'; // Novices
+        }
+        else if (r < 1400) {
+            return 'CD'; // Class D
+        }
+        else if (r < 1600) {
+            return 'CC'; // Class C
+        }
+        else if (r < 1800) {
+            return 'CB'; // Class B
+        }
+        else if (r < 2000) {
+            return 'CA'; // Class A
+        }
+        else if (r < 2200) {
+            return 'CM-USA'; // USA Cadidate Master
+        }
+        else if (r < 2300) {
+            return 'CM'; // Candidate Master
+        }
+        else if (r < 2400) {
+            return 'FM'; // FIDE Master
+        }
+        else if (r < 2500) {
+            return 'IM'; // International Master
+        }
+        else if (r < 2700) {
+            return 'GM'; // GrandMaster
+        }
+        else {
+            return 'SGM'; // (informal title) Super GrandMaster
+        }
     }
 }
-const Joe = {
-    id: 216,
-    name: 'Joe Matune',
-    rating: {
-        category: 'CB',
-        value: 2160
-    }
-};
+const joe = new CreatePlayer('Joe', 1616);
 const magnus = new CreatePlayer('Magnus', 2845);
-console.log(Joe, '\n', magnus, '\n', magnus.isRatingValue);
+console.log(joe, '\n', joe.ratingCategory, '\n', magnus, '\n', magnus.ratingCategory);
 //# sourceMappingURL=index.js.map
